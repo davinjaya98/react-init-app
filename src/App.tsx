c7450733c39f3eff68f3975098a3718c72797c09
Davin Jaya <davin.jaya@dcatalyst.biz>
2020-01-16 11:01:51 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import InitialApp from './components/InitialApp/InitialApp';
+
+const App: React.FC = () => {
+  return (
+    <InitialApp />
+  );
+}
+
+export default App;
